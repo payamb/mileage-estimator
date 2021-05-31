aeb3bce719a69205c50ec1eefca0f1eaaf67d74f
Payam <p.babaee@live.com>
2021-05-31 22:58:44 +0100

    Assert all Vehicle properties are initialised correctly

--- test/model/vehicleBuilder.test.ts
+++ test/model/vehicleBuilder.test.ts
@@ -3,8 +3,20 @@ import { VehicleBuilder } from '../../src/model/vehicleBuilder';
 
 describe('VehicleBuilder', () => {
   it('Should compose properties and build a Vehicle object', () => {
-    const vehicle =  new VehicleBuilder().build();
+    const regDate = new Date();
+    const vehicle = new VehicleBuilder()
+      .withId(123)
+      .withMake('Ford')
+      .withModel('1989')
+      .withVrm('a1233b')
+      .withFirstRegistrationDate(regDate)
+      .build();
 
-    expect(vehicle instanceof Vehicle).toBe(true)
+    expect(vehicle instanceof Vehicle).toEqual(true);
+    expect(vehicle.id).toEqual(123);
+    expect(vehicle.make).toEqual('Ford');
+    expect(vehicle.model).toEqual('1989');
+    expect(vehicle.vrm).toEqual('a1233b');
+    expect(vehicle.firstRegistrationDate).toEqual(regDate);
   });
 });
