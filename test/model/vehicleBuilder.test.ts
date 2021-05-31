f7a663b4e1033b67939d57f120d2ff2eee12c52b
Payam <p.babaee@live.com>
2021-05-31 22:54:49 +0100

    Update test

--- test/model/vehicleBuilder.test.ts
+++ test/model/vehicleBuilder.test.ts
@@ -3,7 +3,8 @@ import { VehicleBuilder } from '../../src/model/vehicleBuilder';
 
 describe('VehicleBuilder', () => {
   it('Should compose properties and build a Vehicle object', () => {
-    const vehicle =  new VehicleBuilder();
+    const vehicle =  new VehicleBuilder().build();
+
     expect(vehicle instanceof Vehicle).toBe(true)
   });
 });
