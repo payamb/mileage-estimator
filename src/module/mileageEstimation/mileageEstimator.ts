ef5a15f5785a18dd270a2f8318fe283b183acaaf
Payam <p.babaee@live.com>
2021-06-02 19:09:12 +0100

    Implement calculateAnnualMileage and pass the test

--- src/module/mileageEstimation/mileageEstimator.ts
+++ src/module/mileageEstimation/mileageEstimator.ts
@@ -3,17 +3,68 @@
 // Estimate the vehicle’s current mileage by projecting from the most recent, event using the average annual mileage
 
 // If there are no timeline events with mileage, calculate using 7,900 miles per year as the average
+
 import { Vehicle } from './../../model/vehicle';
+import { VehicleEventInterface } from './../../model/vehicleEventInterfce';
 import { MileageEstimatorInterface } from './mileageEstimatorInterface';
 
 export class MileageEstimator implements MileageEstimatorInterface {
   vehicle: Vehicle;
+  defaultAnnualMileage = 7900;
 
   createFrom(vehicle: Vehicle) {
     return Object.assign(this, { vehicle: vehicle });
   };
 
   calculateAnnualMileage(): number {
-    return 0;
+    const events = this.vehicle.events;
+
+    events.sort((a: VehicleEventInterface, b: VehicleEventInterface) => {
+      return a.date.getTime() - b.date.getTime();
+    });
+
+    const mileageByYear = this.groupMileageByYear(events, this.defaultAnnualMileage);
+    const totalMileage = mileageByYear.reduce(
+      (accumulator, currentValue, index, source) => {
+        return (accumulator ?? 0) + (currentValue ?? 0)
+      }, 0);
+
+    const averageMileage = this.divideMileageByNofYears(totalMileage ?? 0, mileageByYear.length);
+
+    return averageMileage;
+  }
+
+  private divideMileageByNofYears(mileage: number, numberOfYears: number): number {
+    if (mileage <= 0) return 0;
+    
+    const average = (mileage / numberOfYears).toFixed(0);
+    return parseInt(average);
+  }
+
+  private getYearRange(from: Date): Array<number> {
+    const currentYear = new Date().getUTCFullYear();
+    const startYear = from.getUTCFullYear();
+
+    return Array((currentYear - startYear) + 1)
+      .fill('')
+      .map((v, idx) => currentYear - idx);
+  }
+
+  private groupMileageByYear(events: Array<VehicleEventInterface>, defaultAnnualMileage: number) {
+    if (events.length <= 0) return [];
+
+    const yearRange = this.getYearRange(events[0].date);
+
+    const output = yearRange.map((year: number) => {
+      const eventsInYear = events
+        .filter((event: VehicleEventInterface) => {
+          return event.date.getFullYear() === year && event.hasMileageData();
+        });
+
+      const mostRecentEvent = eventsInYear.pop();
+      return mostRecentEvent ? mostRecentEvent.mileage : defaultAnnualMileage;
+    });
+
+    return output;
   }
 }
--- test/model/module/mileageEstimation/mileageEstimator.test.ts
+++ test/model/module/mileageEstimation/mileageEstimator.test.ts
@@ -1,10 +1,10 @@
 import { Vehicle } from './../../../../src/model/vehicle';
 import { MileageEstimator } from '../../../../src/module/mileageEstimation/mileageEstimator';
 import { AdvertisedForSaleEvent } from './../../../../src/model/event/advertisedForSale';
-// import { MotTestEvent } from './../../src/model/event/MotTest';
+import { MotTestEvent } from './../../../../src/model/event/MotTest';
 
 describe('EstimateMileage Module', () => {
-  it('Should calculate average annual mileage', () => {
+  it('Should calculate average annual mileage correctly with events available for every year', () => {
 
     const vehicle = new Vehicle({
       id: 123,
@@ -15,15 +15,24 @@ describe('EstimateMileage Module', () => {
       events: []
     });
 
-    const advertisedForSaleEvent = new AdvertisedForSaleEvent();
-    
-    vehicle
-      .events
-      .push(advertisedForSaleEvent);
+    vehicle.events.push(new AdvertisedForSaleEvent({
+      date: new Date('2020-12-10T03:24:00'),
+      mileage: 500
+    }));
+
+    vehicle.events.push(new AdvertisedForSaleEvent({
+      date: new Date('2020-10-01T03:24:00'),
+      mileage: 200
+    }));
+
+    vehicle.events.push(new MotTestEvent({
+      date: new Date('2021-05-01T01:01:00'),
+      mileage: 1000
+    }));
 
     const estimator = new MileageEstimator()
       .createFrom(vehicle);
 
-    expect(estimator.calculateAnnualMileage()).toEqual(0);
+    expect(estimator.calculateAnnualMileage()).toEqual(750);
   });
 });
