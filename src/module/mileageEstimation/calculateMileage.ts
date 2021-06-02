b8678a3a48bb8e54e587985bb35c1b531c8d7921
Payam <p.babaee@live.com>
2021-06-02 17:13:25 +0100

    create mileageEstimation module and test

---
+++ src/module/mileageEstimation/calculateMileage.ts
@@ -0,0 +1,6 @@
+// Calculate the average annual mileage using the events in the timeline
+
+// Estimate the vehicle’s current mileage by projecting from the most recent, event using the average annual mileage
+
+// If there are no timeline events with mileage, calculate using 7,900 miles per year as the average
+
