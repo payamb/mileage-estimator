0122ce9bdc7030572fcf6831bdac272bbd5642cb
Payam <p.babaee@live.com>
2021-06-02 17:38:29 +0100

    Implement MileageEstimator enough to pass the test

--- src/module/mileageEstimation/mileageEstimator.ts
+++ src/module/mileageEstimation/mileageEstimator.ts
@@ -3,7 +3,19 @@
 // Estimate the vehicle’s current mileage by projecting from the most recent, event using the average annual mileage
 
 // If there are no timeline events with mileage, calculate using 7,900 miles per year as the average
+import { Vehicle } from './../../model/vehicle';
+import { MileageEstimatorInterface } from './mileageEstimatorInterface';
 
-export class MileageCalculator implements MileageEstimatorInterface {
+export class MileageEstimator implements MileageEstimatorInterface {
+  vehicle: Vehicle;
 
+  createFrom(vehicle: Vehicle) {
+    return Object.assign(this, {vehicle: vehicle});
+    // this.vehicle = vehicle;
+    // return this;
+  };
+
+  calculateAnnualMileage(): number {
+    return 0;
+  }
 } 
